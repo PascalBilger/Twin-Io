a5cbcb618a126fe5457bd8f8903a878b8e2e6f02
Pascal Bilger <pascal.bilger@bluewin.ch>
2024-03-19 00:27:57 +0100

    Changed the compiler optimization from none to fast Code is now able to read the ADC at 2kHz and send it via 3MBaud to Serial. Attention: There is a bug which was not further investigated which mirrors the channels 1-3 from ADC 1 to channels 1-3 of ADC 2

--- Firmware/Prototype/Twin_Io/Core/Src/main.c
+++ Firmware/Prototype/Twin_Io/Core/Src/main.c
@@ -18,6 +18,7 @@
 /* USER CODE END Header */
 /* Includes ------------------------------------------------------------------*/
 #include "main.h"
+#include "dma.h"
 #include "spi.h"
 #include "tim.h"
 #include "usart.h"
@@ -66,6 +67,8 @@ void v_SelectAdc(uint8_t u8_AdcNum, uint8_t on);
 uint8_t u8_ADCReadReg(uint8_t u8_AdcNum, uint8_t u8_RegAddr);
 uint8_t u8_ADCWriteReg(uint8_t u8_AdcNum, uint8_t u8_RegAddr, uint8_t u8_Val);
 uint8_t u8_ADCCommand(uint8_t u8_AdcNum, uint16_t u16_Command);
+uint8_t u8_AllADCCommand(uint16_t u16_Command);
+uint8_t u8_AllADCWriteReg(uint8_t u8_RegAddr, uint8_t u8_Val);
 
 /* USER CODE END PFP */
 
@@ -102,26 +105,48 @@ int main(void)
 
   /* Initialize all configured peripherals */
   MX_GPIO_Init();
+  MX_DMA_Init();
   MX_SPI1_Init();
   MX_USART3_UART_Init();
   MX_TIM2_Init();
+  MX_USART1_UART_Init();
   /* USER CODE BEGIN 2 */
+  HAL_UART_MspInit(&huart1);
 
   HAL_TIM_PWM_Start(&htim2, TIM_CHANNEL_2);	//Start the ADC clock generation
-  HAL_Delay(100);
-  u8_ADCCommand(0, ADC_CMD_RESET);	//Reset the ADC to POR
-  HAL_Delay(100);
-  u8_ADCCommand(0, ADC_CMD_UNLOCK);	//Unlock the ADC registers so they can be written to
-  HAL_Delay(100);
-  u8_ADCWriteReg(0, 0xF, 0xF);		//Start all ADC Channels
-  HAL_Delay(100);
-  u8_ADCWriteReg(0, 0xE, 0b01000000); //Select the slowest conversion mode
-  HAL_Delay(100);
 
-  u8_ADCCommand(0, ADC_CMD_WAKEUP);	//Wake the ADC up and start measuring
+  //Init all ADCs
   HAL_Delay(100);
-  u8_ADCCommand(0, ADC_CMD_UNLOCK);	//Lock the ADC registers so they cant be written to
+  u8_AllADCCommand(ADC_CMD_RESET);	//Reset the ADC to POR
   HAL_Delay(100);
+  u8_AllADCCommand(ADC_CMD_NULL);	//Wake up the SPI interface (See datasheet P37)
+  HAL_Delay(100);
+  u8_AllADCCommand(ADC_CMD_UNLOCK);	//Unlock the ADC registers so they can be written to
+  HAL_Delay(100);
+  u8_AllADCWriteReg(0xF, 0xF);		//Enable all ADC Channels
+  HAL_Delay(100);
+  //u8_AllADCWriteReg(0xD, 0b00000100);		//Set fIclk = fclk/4
+  HAL_Delay(100);
+  u8_AllADCWriteReg(0xE, 0b00100101);		//Set fMod = fIclk/2 and fdata=fmod/512
+  HAL_Delay(100);
+  u8_AllADCWriteReg(0x11, 0b00000000); //Select gain 2x for channel 0
+  HAL_Delay(100);
+  u8_AllADCWriteReg(0x12, 0b00000000); //Select gain 2x for channel 1
+  HAL_Delay(100);
+  u8_AllADCWriteReg(0x13, 0b00000000); //Select gain 2x for channel 2
+  HAL_Delay(100);
+  u8_AllADCWriteReg(0x14, 0b00000000); //Select gain 2x for channel 3
+  HAL_Delay(100);
+  u8_AllADCWriteReg(0xB, 0b01101000);
+  HAL_Delay(100);
+  u8_AllADCWriteReg(0xB, 0b00111100);
+  HAL_Delay(100);
+  u8_AllADCCommand(ADC_CMD_WAKEUP);	//Wake the ADC up and start measuring
+  HAL_Delay(100);
+  u8_AllADCCommand(ADC_CMD_LOCK);	//Lock the ADC registers so they cant be written to
+
+
+
 
   /* USER CODE END 2 */
 
@@ -134,25 +159,44 @@ int main(void)
 	  {
 		  if(pu8_Adc0Rdy[i])
 		  {
-			  pu8_Adc0Rdy[i] = 0; //Reset the Flag
-
-			  uint8_t u8_Err = u8_ADCReadReg(i, 0x05);
-			  HAL_UART_Transmit(&huart3, &u8_Err,1,100);
-
 			  v_SelectAdc(i, 1); // Select the ADC
 
-			  uint32_t pu32_AdcChannels[4];
+			  pu8_Adc0Rdy[i] = 0; //Reset the Flag
 
-			  HAL_UART_Transmit(&huart3, &i, 1, 100);	//Transmit the current ADC number which the samples are from
-			  u32_AdcTransmitReceive(&hspi1, 0);	//Receive and discard the status byte
+			  uint8_t pu8_UartBuf[14];
+			  uint8_t u8_UartBufCnt=0;			//init the counter which keeps track of the number of bits to be sent
 
-			  for(uint8_t j = 0; j < 4; j++)
+			  pu8_UartBuf[u8_UartBufCnt] = i;	//Transmit the current ADC number which the samples are from
+			  u8_UartBufCnt++;
+
+			  for(uint8_t j=0;j<15;j++)//output bytes 3-14 because 0-2 are status bytes
 			  {
-				  pu32_AdcChannels[j] = u32_AdcTransmitReceive(&hspi1, 0);//Get the values from the ADC
-				  HAL_UART_Transmit(&huart3, (uint8_t*)(&pu32_AdcChannels[j]), 3, 100);//Interpret the 32 bit integer as a 8bit array so the single bits are individually shifted out.
+				  while (((SPI1->SR)&(1<<7))) {};  // wait for BSY bit to Reset -> This will indicate that SPI is not busy in communication
+				  SPI1->DR = 0;	//send a dummy byte to receive next byte
+				  while (!((SPI1->SR) &SPI_SR_RXNE)){};  // Wait for RXNE to set -> This will indicate that the Rx buffer is not empty
+
+				  if(j>=3)
+				  {
+					 pu8_UartBuf[u8_UartBufCnt] = SPI1->DR;	//Transmit the current ADC number which the samples are from
+					 u8_UartBufCnt++;
+				  }
+				  else
+				  {
+					  SPI1->DR; //Discard the SPI data
+				  }
 			  }
-			  char data = '\n';
-			  HAL_UART_Transmit(&huart3, (uint8_t*)&data, 1, 100);//Interpret the 32 bit integer as a 8bit array so the single bits are individually shifted out.
+			  while (((SPI1->SR)&(1<<7))) {};  // wait for BSY bit to Reset -> This will indicate that SPI is not busy in communication
+			  pu8_UartBuf[u8_UartBufCnt] = '\n';	//Transmit the current ADC number which the samples are from
+			  u8_UartBufCnt++;
+			  HAL_StatusTypeDef err;
+			  while(huart1.gState != HAL_UART_STATE_READY);
+			  err = HAL_UART_Transmit_DMA(&huart1, pu8_UartBuf, u8_UartBufCnt);
+			  if(err!=HAL_OK)
+			  {
+				  __NOP();
+			  }
+
+			  __HAL_UART_DISABLE_IT(&huart1, UART_IT_TXE);
 			  v_SelectAdc(i, 0); //Deselect the ADC
 
 		  }
@@ -177,12 +221,13 @@ void SystemClock_Config(void)
   /** Initializes the RCC Oscillators according to the specified parameters
   * in the RCC_OscInitTypeDef structure.
   */
-  RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSI;
+  RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSE;
+  RCC_OscInitStruct.HSEState = RCC_HSE_ON;
+  RCC_OscInitStruct.HSEPredivValue = RCC_HSE_PREDIV_DIV2;
   RCC_OscInitStruct.HSIState = RCC_HSI_ON;
-  RCC_OscInitStruct.HSICalibrationValue = RCC_HSICALIBRATION_DEFAULT;
   RCC_OscInitStruct.Prediv1Source = RCC_PREDIV1_SOURCE_HSE;
   RCC_OscInitStruct.PLL.PLLState = RCC_PLL_ON;
-  RCC_OscInitStruct.PLL.PLLSource = RCC_PLLSOURCE_HSI_DIV2;
+  RCC_OscInitStruct.PLL.PLLSource = RCC_PLLSOURCE_HSE;
   RCC_OscInitStruct.PLL.PLLMUL = RCC_PLL_MUL8;
   RCC_OscInitStruct.PLL2.PLL2State = RCC_PLL_NONE;
   if (HAL_RCC_OscConfig(&RCC_OscInitStruct) != HAL_OK)
@@ -196,10 +241,10 @@ void SystemClock_Config(void)
                               |RCC_CLOCKTYPE_PCLK1|RCC_CLOCKTYPE_PCLK2;
   RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_PLLCLK;
   RCC_ClkInitStruct.AHBCLKDivider = RCC_SYSCLK_DIV1;
-  RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV1;
+  RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV2;
   RCC_ClkInitStruct.APB2CLKDivider = RCC_HCLK_DIV1;
 
-  if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_1) != HAL_OK)
+  if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_2) != HAL_OK)
   {
     Error_Handler();
   }
@@ -270,7 +315,7 @@ uint8_t u8_ADCReadReg(uint8_t u8_AdcNum, uint8_t u8_RegAddr)
 
 	uint8_t pu8_RecvBuf[3];
 	uint8_t pu8_ZeroBuf[3] = {0,0,0};
-	HAL_SPI_TransmitReceive(&hspi1, &pu8_ZeroBuf, &pu8_RecvBuf, 3, 100);
+	HAL_SPI_TransmitReceive(&hspi1, pu8_ZeroBuf, pu8_RecvBuf, 3, 100);
 	v_SelectAdc(u8_AdcNum, 0);
 	return pu8_RecvBuf[1];
 
@@ -290,6 +335,16 @@ uint8_t u8_ADCWriteReg(uint8_t u8_AdcNum, uint8_t u8_RegAddr, uint8_t u8_Val)
 	return 0;
 }
 
+uint8_t u8_AllADCWriteReg(uint8_t u8_RegAddr, uint8_t u8_Val)
+{
+	for(uint8_t i = 0; i < 3; i++)
+	{
+		u8_ADCWriteReg(i, u8_RegAddr, u8_Val);
+		HAL_Delay(10);
+	}
+	return 0;
+}
+
 uint8_t u8_ADCCommand(uint8_t u8_AdcNum, uint16_t u16_Command)
 {
 	uint8_t u8_MSB = u16_Command >> 8;
@@ -300,6 +355,17 @@ uint8_t u8_ADCCommand(uint8_t u8_AdcNum, uint16_t u16_Command)
 	uint8_t u8_Zero = 0;
 	HAL_SPI_Transmit(&hspi1, &u8_Zero, 1, 100);
 	v_SelectAdc(u8_AdcNum, 0);
+	return 0;
+}
+
+uint8_t u8_AllADCCommand(uint16_t u16_Command)
+{
+	for(uint8_t i = 0; i < 3; i++)
+	{
+		u8_ADCCommand(i, u16_Command);
+		HAL_Delay(10);
+		u8_ADCCommand(i, ADC_CMD_NULL);//Send a NULL command to read back the data
+	}
 	return 0;
 }
 
